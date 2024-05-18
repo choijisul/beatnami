df19bdaf5658504dc3254f7a43204df6ef27371c
user.email.w2217@e-mirim.hs.kr <user.email.w2217@e-mirim.hs.kr>
2024-05-18 16:58:27 +0900

    랭킹관련 이미지 업데이트, 랭킹 페이지 front 완성

--- src/components/RankingScreen.js
+++ src/components/RankingScreen.js
@@ -3,9 +3,47 @@ import './css/RankingScreen.css';
 
 const RankingScreen = ({ ScreenName }) => {
   return (
-    <div className="ranking-screen">
+    <div className='ranking-screen'>
       <button className='back-button' onClick={ScreenName}></button>
-      <button onClick={ScreenName}>게임 화면 넘어감</button>
+      <div className='ranking-bord'>
+        <div className='ranking-div'>
+          <div className='ranking-text-div'>
+            <h3 className='no'>1</h3>
+            <h5 className='nickname'>닉네임</h5>
+            <h5 className='score'>점수</h5>
+          </div>
+          <div className='ranking-text-div'>
+            <h3 className='no'>2</h3>
+            <h5 className='nickname'>닉네임</h5>
+            <h5 className='score'>점수</h5>
+          </div>
+          <div className='ranking-text-div'>
+            <h3 className='no'>3</h3>
+            <h5 className='nickname'>닉네임</h5>
+            <h5 className='score'>점수</h5>
+          </div>
+          <div className='ranking-text-div'>
+            <h3 className='no'>4</h3>
+            <h5 className='nickname'>닉네임</h5>
+            <h5 className='score'>점수</h5>
+          </div>
+          <div className='ranking-text-div'>
+            <h3 className='no'>5</h3>
+            <h5 className='nickname'>닉네임</h5>
+            <h5 className='score'>점수</h5>
+          </div>
+          <div className='ranking-text-div'>
+            <h3 className='no'>6</h3>
+            <h5 className='nickname'>닉네임</h5>
+            <h5 className='score'>점수</h5>
+          </div>
+          <div className='ranking-text-div'>
+            <h3 className='no'>7</h3>
+            <h5 className='nickname'>닉네임</h5>
+            <h5 className='score'>점수</h5>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
