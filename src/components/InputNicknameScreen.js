969626ee1d53fc095ad209a75657bd57ae775396
user.email.w2217@e-mirim.hs.kr <user.email.w2217@e-mirim.hs.kr>
2024-05-18 15:08:45 +0900

    뒤로가기버튼 이미지 추가, 닉네임 입력 페이지 완성

--- src/components/InputNicknameScreen.js
+++ src/components/InputNicknameScreen.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import './css/InputNicknameScreen.css';
 
 const InputNicknameScreen = ({ ScreenName }) => {
   return (
-    <div className="inputNickname">
-      <h1>닉네임 입력</h1>
-      <button onClick={ScreenName}>스토리 화면 넘어감</button>
+    <div className='nickname-screen'>
+      <div className='input-border-div'>
+        <div className='input-title'>닉네임</div>
+        <div className='input-div'>
+          <input type='text' className='input' placeholder='닉네임을 입력해주세요' />
+        </div>
+      </div>
+      <button className='goStory-button' onClick={ScreenName}>확인</button>
     </div>
   );
 };
-
 
 export default InputNicknameScreen;
