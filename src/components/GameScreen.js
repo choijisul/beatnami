e6f76d41fad54d82b1308288154f58094abfe08a
박도연 <121646938+gitdoyeon@users.noreply.github.com>
2024-05-25 21:30:57 +0900

    태초마을

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -54,7 +54,7 @@ const GameScreen = ({ ScreenName, GoBackClick }) => {
     <div className='game-screen'>
       <button className='back-button' onClick={GoBackClick}></button>
       <button className='' onClick={ScreenName}></button>    {/* 다음으로 넘어가는 버튼 */}
-        <canvas ref={canvasRef}></canvas>
+      <canvas ref={canvasRef}></canvas>/
     </div>
   )
 }
