ae0487fd7a00b711a20a8c4fa7a4f8f84eb0e607
박도연 <121646938+gitdoyeon@users.noreply.github.com>
2024-05-25 20:52:57 +0900

    에러 해결!

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -50,7 +50,6 @@ const GameScreen = ({ ScreenName, GoBackClick }) => {
     }
   }, []);
 
-  
   return (
     <div className='game-screen'>
       <button className='back-button' onClick={GoBackClick}></button>
