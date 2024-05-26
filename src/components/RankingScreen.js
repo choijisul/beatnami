380a695fca7f669b8533d014037fa8e729ebdbc5
최지설 <121646920+choijisul@users.noreply.github.com>
2024-05-26 16:45:04 +0900

    랭킹 디자인 수정

--- src/components/RankingScreen.js
+++ src/components/RankingScreen.js
@@ -10,13 +10,15 @@ const RankingScreen = ({ ScreenName }) => {
       if (index < 7) { // 7위까지 출력
         return (
           <div key={index} className='ranking-text-div'>
-            <h3 className='no'>{index + 1}</h3>
-            <h5 className='nickname'>{user.nickname}</h5> 
-            <h5 className='score'>{user.score}</h5> 
+            <div className='noNickname'>
+              <h3 className='no'>{index + 1}</h3>
+              <h5 className='nickname'>{user.nickname}</h5>
+            </div>
+            <h5 className='score'>{user.score}</h5>
           </div>
         );
       } else {
-        return null; 
+        return null;
       }
     });
   };
@@ -28,11 +30,11 @@ const RankingScreen = ({ ScreenName }) => {
 
     // 특정 시간에 localstorage값 지워지게.  //
     if (currentHour === 10 && currentMinute === 20) {
-        localStorage.removeItem('nickname');
-        localStorage.removeItem('score');
-        localStorage.removeItem('users');   
+      localStorage.removeItem('nickname');
+      localStorage.removeItem('score');
+      localStorage.removeItem('users');
     }
-};
+  };
 
   return (
     <div className='ranking-screen'>
