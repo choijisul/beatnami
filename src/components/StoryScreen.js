612146ee9d490fafc551c2bd692b0c880fa33a9d
kimmyounha <w2224@e-mirim.hs.kr>
2024-06-02 10:45:28 +0900

    init commit

--- src/components/StoryScreen.js
+++ src/components/StoryScreen.js
@@ -3,51 +3,59 @@ import './css/StoryScreen.css';
 import dialoguesData from '../assets/dialog.json'; // JSON 파일 경로
 
 const StoryScreen = ({ ScreenName, nickname }) => {
+  const [currentScene, setCurrentScene] = useState('trainstation_scene');
   const [currentDialogueIndex, setCurrentDialogueIndex] = useState(0);
   const [currentText, setCurrentText] = useState('');
   const [isAnimating, setIsAnimating] = useState(false);
   const [dialogues, setDialogues] = useState([]);
-  const currentInterval = useRef(null); // useRef로 currentInterval 정의
+  const currentInterval = useRef(null);
 
   useEffect(() => {
-    // JSON 데이터를 state에 설정
-    setDialogues(dialoguesData);
-  }, []);
+    // 현재 씬의 대사 데이터를 state에 설정
+    setDialogues(dialoguesData[currentScene]);
+  }, [currentScene]);
 
   const startAnimation = (dialogue) => {
     setIsAnimating(true);
     setCurrentText('');
     let index = 0;
     const interval = setInterval(() => {
-      setCurrentText((prev) => prev + dialogue[index]);
+      setCurrentText((prev) => (prev !== undefined ? prev + dialogue.charAt(index) : dialogue.charAt(index)));
       index++;
       if (index === dialogue.length) {
         clearInterval(interval);
         setIsAnimating(false);
       }
-    }, 50); // 글자 출력 속도 조절
-    currentInterval.current = interval; // currentInterval에 interval 저장
+    }, 50);
+    currentInterval.current = interval;
   };
 
   const handleDialogueClick = useCallback(() => {
     if (isAnimating) {
-      // 현재 애니메이션 중이면 대사를 모두 출력
+      // 애니메이션 중이면 대사를 모두 출력
       clearInterval(currentInterval.current);
       setCurrentText(dialogues[currentDialogueIndex].dialogue);
       setIsAnimating(false);
     } else {
-      // 애니메이션이 끝났다면 다음 대사로
-      if (currentDialogueIndex < dialogues.length - 1) {
-        setCurrentDialogueIndex(currentDialogueIndex + 1);
-      } else {
+      // 애니메이션이 끝났다면 다음 대사로 이동
+      const nextDialogue = dialogues[currentDialogueIndex]?.next;
+      if (nextDialogue === null) {
+        // 스토리가 끝난 경우
         ScreenName();
+      } else if (typeof nextDialogue === 'string') {
+        // 다음 씬으로 전환
+        setCurrentScene(nextDialogue);
+        setCurrentDialogueIndex(0);
+      } else if (typeof nextDialogue === 'number') {
+        // 현재 씬의 다음 대사로 이동
+        setCurrentDialogueIndex(nextDialogue);
       }
     }
   }, [isAnimating, currentDialogueIndex, dialogues, ScreenName]);
 
   useEffect(() => {
     if (dialogues.length > 0) {
-      startAnimation(dialogues[currentDialogueIndex].dialogue);
+      startAnimation(dialogues[currentDialogueIndex].dialogue || '');
     }
   }, [currentDialogueIndex, dialogues]);
 
